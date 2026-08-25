1f0a6cc088b132e2a1405ae75952bc878717217c
hugocoimbrac <87131049+hugocoimbrac@users.noreply.github.com>
2026-08-25 16:41:58 +0100

    Add GotAll visual theme

---
+++ src/theme/index.ts
@@ -0,0 +1,2 @@
+export const colors={background:'#F9FAF5',card:'#FFFFFF',text:'#17211A',muted:'#69736C',line:'#E4E8E2',green:'#68D475',greenDark:'#36AA50',greenSoft:'#EAF9E9',danger:'#D9534F',black:'#101512'};
+export const radius={sm:14,md:18,lg:24,xl:32};
